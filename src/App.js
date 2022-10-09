10d9bdee77dddc092cb66904c591d9b76b243ae5
Pablo Villagran <pavillagran.dwfsc1@bootcampudd.cl>
2022-10-09 00:36:56 -0300

    se borraron archivos y carpetas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+
+import Inicio from './pages/Inicio'
+import Menu from './pages/Menu'
+import Contacto from './pages/Contacto'
+import PageNotFound from './pages/PageNotFound'
+import Informacion from './pages/Informacion';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Routes>
+        <Route path='/' element={<Inicio /> } />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/contacto' element={<Contacto />} />
+        <Route path='/informacion' element={<Informacion />} />
+        <Route path="*" element={<PageNotFound />} />
+        </Routes>
+ 
+
   );
 }
 
